190097b71e46ebeb3f8ce010a3d66d1c05fecd12
Kendry Grullón <kengrullon@gmail.com>
2025-09-19 14:35:50 -0400

    advancing some.

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
+void print_board_state(map<int, char> state)
+{
+  cout << state[1] << " | " << state[2] << " | " << state[3] << endl;
+  cout << "-   -   -" << endl;
+  cout << state[4] << " | " << state[5] << " | " << state[6] << endl;
+  cout << "-   -   -" << endl;
+  cout << state[7] << " | " << state[8] << " | " << state[9] << endl;
+}
+
 int main()
 {
-  cout << "hola mundo" << endl;
+  map<int, char> state = {{1, '.'}, {2, '.'}};
+  for (int i = 1; i < 10; i++)
+  {
+    state.insert{{i, '.'}};
+  }
+
+  print_board_state(state);
   return 0;
 }
